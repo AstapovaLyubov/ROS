5ccdb6ad1c078cebdd816054352a64eaef7cbaf7
Астапова Любовь <43094174+AstapovaLyubov@users.noreply.github.com>
2020-11-10 17:52:31 +0300

    Update server.cpp

--- src/task2/src/server.cpp
+++ src/task2/src/server.cpp
@@ -2,7 +2,6 @@
 #include <task2/GetDeterminant.h>
 #include <cstdlib>
 #include <iostream>
-
 #include <Eigen/Eigen>
 
 
@@ -21,18 +20,14 @@ int main(int argc, char **argv)
     return 0;
 }
 
+
 bool get_determinant(task2::GetDeterminant::Request &req,
                       task2::GetDeterminant::Response &resp)
 {
     Eigen::Matrix<float, 3, 3> X;
     for (int i = 0; i<X.rows(); i++)
-    {
         for (int j = 0; j<X.cols(); j++)
-        {
             X(i,j) = req.input_matrix[i*X.cols()+j];
-        }
-        
-    }
 
     resp.det = X.determinant();
     ROS_INFO_STREAM("Matrix: \n" << X);
